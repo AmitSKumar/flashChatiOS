4492259889787f20fedbe7d7b3fe82609745e2b5
Amit Singh <amitsighbxr@gmail.com>
2023-09-09 17:02:57 +0800

    final commit flas chat app

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+import FirebaseCore
+import FirebaseFirestore
+import IQKeyboardManagerSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        print(db)
         return true
     }
 
---
+++ Flash Chat iOS13/constants.swift
@@ -0,0 +1,28 @@
+//
+//  constants.swift
+//  Flash Chat iOS13
+//
+//  Created by user243065 on 9/4/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    static let appName = "⚡️FlashChat"
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
